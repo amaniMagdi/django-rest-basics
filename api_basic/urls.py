1d70010842e5dc053fbaf6e5a59f17f2bdec45c6
Amani Magdi Farg <amanim.farag@gmail.com>
2024-01-20 12:11:06 +0200

    REST Class Based API Views

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,18 @@
 
 from django.urls import path
-from .views import article_list, article_detail
+#from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
+from .views import ArticleAPIView, ArticleDetails
 
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>/', article_detail)
+    #######
+    # REST Function Based API Views &  api_view() Decorator 
+    #######
+    # path('article/', article_list),
+    # path('detail/<int:pk>/', article_detail)
+
+    #########
+    # REST Class Based API Views
+    #########
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetails.as_view())
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -7,6 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 
 '''
@@ -42,7 +43,7 @@ from rest_framework import status
     
 #     elif request.method == "PUT":
 #         data = JSONParser().parse(request)
-#         serializer = ArticleSerializer(data=data)
+#         serializer = ArticleSerializer(article, data=data)
 
 #         if serializer.is_valid():
 #             serializer.save()
@@ -57,14 +58,56 @@ from rest_framework import status
  REST Framework api_view() Decorator
 '''
 
-@api_view(['GET', 'POST'])
-def article_list(request):
-    if request.method == "GET":
+# @api_view(['GET', 'POST'])
+# def article_list(request):
+#     if request.method == "GET":
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == "POST":
+#         serializer = ArticleSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def article_detail(request, pk):
+#     try:
+#         article = Article.objects.get(pk=pk)
+
+#     except Article.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == "GET": 
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+    
+#     elif request.method == "PUT":
+#         serializer = ArticleSerializer(article, data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == "DELETE":
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+'''
+ REST Class Based API Views
+'''
+
+class ArticleAPIView(APIView):
+    def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self, request):
         serializer = ArticleSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -72,30 +115,39 @@ def article_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def article_detail(request, pk):
-    try:
-        article = Article.objects.get(pk=pk)
+class ArticleDetails(APIView):
 
-    except Article.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
 
-    if request.method == "GET": 
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        print(article)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
-    
-    elif request.method == "PUT":
-        serializer = ArticleSerializer(data=request.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    def delete(self, request, id):
+        article = self.get_object(id)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
     
+
+
         
 
 
